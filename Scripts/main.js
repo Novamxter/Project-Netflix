da0a1dca3348019376ed8ddc64b762ed464d9a6d
Novamxter <mehramkmohit968@gmail.com>
2025-03-09 11:34:37 +0530

    Testing deployment

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,5 @@
 import {langDropdown} from "./Drop-down.js";
-import {handleScroll} from "./Movie-Scroll.js";
+import {handleScroll} from "./Movie-scroll.js";
 import {handleAnswer} from "./handle-answer.js";
 import {renderDigits} from "./digits.js";
 document.addEventListener("DOMContentLoaded",function(){
